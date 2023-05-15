1247eb6fd36cd5e96c5d9847ee215f0233647e2a
John-John Tedro <udoprog@tedro.se>
2023-05-15 21:59:46 +0200

    Minimal reproduction for #2

---
+++ examples/decode_conflict.rs
@@ -0,0 +1,8 @@
+use musli::Decode;
+
+#[derive(bitcode::Decode)]
+struct Struct {
+    field: u64,
+}
+
+fn main() {}
---
+++ examples/encode_conflict.rs
@@ -0,0 +1,8 @@
+use musli::Encode;
+
+#[derive(bitcode::Encode)]
+struct Struct {
+    field: u64,
+}
+
+fn main() {}
